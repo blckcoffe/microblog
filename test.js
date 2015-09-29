afd7c8df1758ecf364f748c8f32c02c0a271be73
shen <da.shen@outlook.com>
2015-09-29 18:32:05 +0800

    Remove hello.cc

--- test.js
+++
@@ -1,8 +0,0 @@
-//var test_cluster = require('./cluster_test.js'); 
-//var data = 0;//这里定义数据不会被所有进程共享，各个进程有各自的内存区域 
-//console.log("Test begin:::::::");
-
-//test_cluster.start_cluster();
-
-var addon = require('./build/Release/hello.node');
-console.log("addon.hello()");
